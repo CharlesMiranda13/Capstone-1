fe4ea181ff6c8092a72cab9e1ff571263fe39236
Charles Miranda <mirandacharles780@gmail.com>
2025-08-15 23:01:44 +0800

    edited loading

--- Public/js/loading.js
+++ Public/js/loading.js
@@ -1,4 +1,5 @@
 setTimeout(function () {
   document.getElementById('loading-screen').style.display = 'none';
   document.getElementById('homepage').style.display = 'block';
+  document.querySelector('main').style.display = 'block';
 }, 3000);
