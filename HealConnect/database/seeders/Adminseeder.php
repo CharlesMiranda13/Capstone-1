231895fa0ed166d121cf27012ab3ff20e9b3ff9d
Charles Miranda <mirandacharles780@gmail.com>
2025-09-17 09:40:04 +0800

    update adminseeder

--- HealConnect/database/seeders/Adminseeder.php
+++ HealConnect/database/seeders/Adminseeder.php
@@ -3,20 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DB;
 
 class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('users')->updateOrInsert(
-            ['email' => 'admin@gmail.com'], // unique key to check
+        User::updateOrCreate(
+            ['email' => env('ADMIN_EMAIL')],
             [
-                'name' => 'Admin',
-                'password' => Hash::make('admin1234'),
-                'role' => 'admin', // required field
-                'email_verified_at' => now(),
+                'name' => env('ADMIN_NAME'),
+                'password' => Hash::make(env('ADMIN_PASSWORD')),
+                'role' => 'admin', 
             ]
         );
     }
