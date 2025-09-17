38d3746fcf882371bab6248ab90d74ce5f6077d4
Charles Miranda <mirandacharles780@gmail.com>
2025-09-17 13:55:17 +0800

    Fix admin seeder

--- HealConnect/database/seeders/Adminseeder.php
+++ HealConnect/database/seeders/Adminseeder.php
@@ -11,10 +11,10 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => env('ADMIN_EMAIL')],
+            ['email' => env('ADMIN_EMAIL', 'admin@gmail.com')],
             [
-                'name' => env('ADMIN_NAME'),
-                'password' => Hash::make(env('ADMIN_PASSWORD')),
+                'name' => env('ADMIN_NAME', 'admin'),
+                'password' => Hash::make(env('ADMIN_PASSWORD', 'admin1234')),
                 'role' => 'admin', 
             ]
         );
