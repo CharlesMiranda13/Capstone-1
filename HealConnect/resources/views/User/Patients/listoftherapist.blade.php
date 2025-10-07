f1d0a0bf2d7f87466d61f6e29e4b1ff139e304d4
Charles Miranda <mirandacharles780@gmail.com>
2025-10-07 09:29:13 +0800

    Enhance therapist listing page with improved styling and layout

--- HealConnect/resources/views/User/Patients/listoftherapist.blade.php
+++ HealConnect/resources/views/User/Patients/listoftherapist.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.patient_layout')
 
 @section('title', 'List of Therapists & Clinics')
+@section('styles')
+<link rel="stylesheet" href="{{ asset('css/patient.css') }}">
+@endsection
 
 @section('content')
-
-<h2 style ="text-align: center">Available Therapists & Clinics</h2>
-<ul>
-    @forelse($therapists as $therapist)
-        <li>
-            {{ $therapist->name }}
-            <span style="color: gray;">({{ ucfirst($therapist->role) }})</span>
-        </li>
-    @empty
-        <li style ="text-align:center"> No therapists or clinics available.</li>
-    @endforelse
-</ul>
+<div class="ptlist">
+    <h2>Available Therapists & Clinics</h2>
+    <ul>
+        @forelse($therapists as $therapist)
+            <li>
+                <p><strong>Therapist/Clinic Name:</strong> {{ $therapist->name }}</p>
+                <p><strong>Therapist/Clinic Email:</strong> {{ $therapist->email }}</p> 
+                <p><strong>Therapist/Clinic Specialization:</strong> {{ $therapist->specialization }}</p>
+                <span>{{ ucfirst($therapist->role) }}</span>
+            </li>
+        @empty
+            <li style="text-align:center;">No therapists or clinics available.</li>
+        @endforelse
+    </ul>
+</div>
 @endsection
