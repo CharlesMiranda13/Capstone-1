e8c68bcdf60ad4bdc01d613046f77ac5e2221381
Charles Miranda <mirandacharles780@gmail.com>
2025-10-28 16:44:51 +0800

    fix pusher key issue in message

--- HealConnect/resources/views/chat/chat.blade.php
+++ HealConnect/resources/views/chat/chat.blade.php
@@ -15,22 +15,79 @@
     }
 @endphp
 
-@extends($layouts)
+@extends($layout)
 
-@section('title', 'Chat')
+@section('title', 'Messages')
+
+@section('styles')
+<link rel="stylesheet" href="{{ asset('css/chat.css') }}">
+@endsection
 
 @section('content')
-<div class="chat-container">
-    <h2 class="chat-title">Chat</h2>
+<div class="chat-page">
+    <div class="chat-container">
 
-    <div id="chat-box" class="chat-box">
-        <!-- Messages  -->
+        {{--  CHAT LIST --}}
+        <div class="chat-sidebar">
+            <div class="sidebar-header">
+                <h3>Chats</h3>
+            </div>
+            <div class="sidebar-search">
+                <input type="text" id="chat-search" placeholder="Search..." autocomplete="off">
+            </div>
+
+            <div class="chat-list" id="chat-list">
+                @forelse ($conversations as $conversation)
+                    <div class="chat-item" data-user-id="{{ $conversation->id }}">
+                        <img src="{{ $conversation->profile_picture ? asset('storage/' . $conversation->profile_picture) : asset('images/default-profile.png') }}" class="avatar" alt="User">
+                        <div class="chat-info">
+                            <h4>{{ $conversation->name }}</h4>
+                            <p>{{ $conversation->latest_message ?? 'Start a conversation...' }}</p>
+                        </div>
+                    </div>
+                @empty
+                    <p class="no-chats">No conversations yet.</p>
+                @endforelse
+            </div>
+        </div>
+
+        {{-- RIGHT SIDE: CHAT AREA --}}
+        <div class="chat-main">
+            <div class="chat-header">
+                <div class="chat-user">
+                    <img src="{{ $user->profile_picture ? asset('storage/' . $user->profile_picture) : asset('images/default-profile.png') }}" class="avatar" alt="User">
+                    <h2 id="chat-username">Select a conversation</h2>
+                </div>
+                <div class="chat-search">
+                    <input type="text" id="message-search" placeholder="Search messages...">
+                </div>
+            </div>
+
+            {{-- MESSAGES DISPLAY --}}
+            <div class="chat-messages" id="chat-messages">
+                <div class="no-chat-selected">
+                    <p>Please select a chat to start messaging.</p>
+                </div>
+            </div>
+
+            {{-- MESSAGE INPUT --}}
+            <div class="chat-input">
+                <form id="chat-form">
+                    @csrf
+                    <input type="text" id="message-input" placeholder="Type your message..." autocomplete="off" required>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </div>
     </div>
-
-    <form id="chat-form" class="chat-form">
-        @csrf
-        <input type="text" id="message-input" placeholder="Type a message..." autocomplete="off">
-        <button type="submit">Send</button>
-    </form>
 </div>
+@endsection
+
+@section('scripts')
+<script>
+    window.userId = {{ auth()->user()->id }};
+    window.pusherKey = "{{ config('broadcasting.connections.pusher.key') }}";
+    window.pusherCluster = "{{ config('broadcasting.connections.pusher.options.cluster') }}"
+</script>
+<script src="{{ asset('js/chat.js') }}"></script>
 @endsection
