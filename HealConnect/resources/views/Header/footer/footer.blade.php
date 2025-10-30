f8d4da9207cc32863f94f4301dd4348b473a505e
Charles Miranda <mirandacharles780@gmail.com>
2025-10-31 07:16:33 +0800

    Update the style and structure of the footer

--- HealConnect/resources/views/Header/footer/footer.blade.php
+++ HealConnect/resources/views/Header/footer/footer.blade.php
@@ -1,3 +1,24 @@
-<footer>
-    <p>&copy; {{ date('Y') }} HealConnect. All rights reserved.</p>  
+<div class="footer-top-bar">
+    <p>Physical HEALTH PROFESSIONAL? ADD YOUR PRACTICE. 
+        <a href="{{url('logandsign') }}" class="get-listed-btn">GET LISTED</a>
+    </p>
+</div>
+
+<!-- Main Footer -->
+<footer class="healconnect-footer">
+    <div class="footer-container">
+        <div class="footer-left">
+                  <img src="{{ asset('images/logo1.png') }}" alt="HealConnect Logo"  class="footer-logo">
+            <p>&copy; {{ date('Y') }} HealConnect. All rights reserved.</p>
+        </div>
+
+        <div class="footer-right">
+            <ul class="footer-links">
+                <li><a href="{{url('/about') }}">About us</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="{{ url('/contact') }}">Contact</a></li>
+            </ul>
+        </div>
+    </div>
 </footer>
