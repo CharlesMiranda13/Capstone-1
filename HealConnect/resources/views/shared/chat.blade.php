a7e8bd073c4562b22fce6834095195d41493b7d4
Charles Miranda <mirandacharles780@gmail.com>
2025-11-11 17:45:59 +0800

    Add voice message functionality with microphone button and styling updates

--- HealConnect/resources/views/shared/chat.blade.php
+++ HealConnect/resources/views/shared/chat.blade.php
@@ -40,7 +40,7 @@
             <div class="chat-list" id="chat-list">
                 @forelse ($conversations as $conversation)
                     <div class="chat-item" data-user-id="{{ $conversation->id }}">
-                        <img src="{{ $conversation->profile_picture ? asset('storage/' . $conversation->profile_picture) : asset('images/default-profile.png') }}" class="avatar" alt="User">
+                        <img src="{{ $conversation->profile_picture ? asset('storage/' . $conversation->profile_picture) : asset('images/logo1.png') }}" class="avatar" alt="User">
                         <div class="chat-info">
                             <h4>{{ $conversation->name }}</h4>
                             <p>{{ Str::limit($conversation->latest_message ?? 'Start a conversation...', 35) }}</p>
@@ -56,7 +56,7 @@
         <section class="chat-main">
             <header class="chat-header">
                 <div class="chat-user">
-                    <img src="{{ asset('images/default-profile.png') }}" id="chat-user-avatar" class="avatar" alt="User">
+                    <img src="{{ asset('images/logo1.png') }}" id="chat-user-avatar" class="avatar" alt="User">
                     <h2 id="chat-username">Select a conversation</h2>
                 </div>
             </header>
@@ -72,6 +72,11 @@
                 <form id="chat-form" autocomplete="off">
                     @csrf
                     <input type="text" id="message-input" placeholder="Type your message..." required>
+
+                    <button type="button" id="record-btn" class="mic-btn" title="voice message">
+                        <i class="fas fa-microphone"></i>
+                </button> 
+
                     <button type="submit" class="send-btn">
                         <i class="fas fa-paper-plane"></i>
                     </button>
@@ -88,6 +93,6 @@
     window.pusherKey = "{{ config('broadcasting.connections.pusher.key') }}";
     window.pusherCluster = "{{ config('broadcasting.connections.pusher.options.cluster') }}";
 </script>
-<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <script src="{{ asset('js/chat.js') }}"></script>
 @endsection
