82f69483795febd16450ac866f73d3765e536390
Charles Miranda <mirandacharles780@gmail.com>
2025-11-13 23:20:54 +0800

    Change MessageSent event to implement ShouldBroadcastNow to enable instant real-time updates for receivers

--- HealConnect/app/Events/MessageSent.php
+++ HealConnect/app/Events/MessageSent.php
@@ -7,10 +7,10 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class MessageSent implements ShouldBroadcast
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
